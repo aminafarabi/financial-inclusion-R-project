e06a09f2a97489dbebe1507e5a946e0749dd63c1
farabiamina <farabi.amina@gmail.com>
2026-01-11 14:48:00 +0100

    demographics

--- scripts/demographics.R
+++ scripts/demographics.R
@@ -9,17 +9,56 @@ cluster_vars <- read.csv("data/cluster_data.csv")
 pca_data <- read.csv("data/pca.csv")
 
 kz_data$km3 <- factor(cluster_vars$km3)
+
+# modifying all attributes to factors with corresponding labels
 kz_data$km3_label <- factor(
   kz_data$km3,
   levels = c(1, 2, 3),
   labels = c(
-    "Basic account holders limited engagement",
+    "Basic account holders with limited digital engagement",
     "Digitally and financially active users",
     "Partially included and irregular users"
   )
 )
 kz_data$km4 <- factor(cluster_vars$km4)
 
+kz_data <- kz_data %>%
+  mutate(
+    inc_q_label = factor(
+      inc_q,
+      levels = c(1, 2, 3, 4, 5),
+      labels = c("Lowest", "Low-middle", "Middle", "Upper-middle", "Highest")
+    )
+  )
+
+kz_data <- kz_data %>%
+  mutate(
+    educ_label = factor(
+      educ,
+      levels = c(1, 2, 3),
+      labels = c("Primary or less", "Secondary", "Tertiary or more")
+    )
+  )
+
+kz_data <- kz_data %>%
+  mutate(
+    urban_label = factor(
+      urbanicity,
+      levels = c(1, 2),
+      labels = c("Rural area", "Urban area")
+    )
+  )
+
+kz_data <- kz_data %>%
+  mutate(
+    emp_label = factor(
+      emp_in,
+      levels = c(1, 2),
+      labels = c("In the workforce", "Out of the workforce")
+    )
+  )
+
+# getting basic cluster distributions of demographic attributes
 demo_binary <- kz_data %>%
   group_by(km3_label) %>%
   summarise(
@@ -49,18 +88,20 @@ demo_fin24 <- kz_data %>%
   mutate(share = n / sum(n)) %>%
   arrange(km3, desc(share))
 
-
+# -----------------------------------------
+# plots
+# -----------------------------------------
 get_gradient <- function(n) {
   colorRampPalette(c(
     "darkgreen",
-    "#cecece"
+    "#728a7b"
   ))(n)
 }
 base_colors <- get_gradient(6)
 
 age_dist_plot <- kz_data %>%
   ggplot(aes(x = age)) +
-  geom_density(fill = "darkgreen", alpha = 0.6) +
+  geom_density(fill = "darkgreen", alpha = 0.8) +
   facet_wrap(~ km3_label, ncol = 1) +
   labs(
     title = "Age distribution by cluster",
@@ -72,17 +113,6 @@ age_dist_plot <- kz_data %>%
     plot.title = element_text(hjust = 0.5)
   )
 
-inc_colors <- base_colors[1:nlevels(kz_data$inc_q_label)]
-
-kz_data <- kz_data %>%
-  mutate(
-    inc_q_label = factor(
-      inc_q,
-      levels = c(1, 2, 3, 4, 5),
-      labels = c("Lowest", "Low-middle", "Middle", "Upper-middle", "Highest")
-    )
-  )
-
 income_dist_plot <- kz_data %>%
   group_by(km3_label, inc_q_label) %>%
   summarise(n = n(), .groups = "drop") %>%
@@ -92,7 +122,7 @@ income_dist_plot <- kz_data %>%
   geom_col(width = 0.8) +
   facet_wrap(~ km3_label, ncol = 1) +
   scale_x_continuous(labels = scales::percent_format()) +
-  scale_fill_manual(values = inc_colors) +
+  scale_fill_manual(values = base_colors) +
   labs(
     title = "Income level distribution by cluster",
     x = "",
@@ -106,17 +136,6 @@ income_dist_plot <- kz_data %>%
     axis.ticks.x = element_blank()
   )
 
-kz_data <- kz_data %>%
-  mutate(
-    educ_label = factor(
-      educ,
-      levels = c(1, 2, 3),
-      labels = c("Primary or less", "Secondary", "Tertiary or more")
-    )
-  )
-
-#educ_colors <- base_colors[1:nlevels(kz_data$inc_q_label)]
-
 educ_dist_plot <-  kz_data %>%
   group_by(km3_label, educ_label) %>%
   summarise(n = n(), .groups = "drop") %>%
@@ -126,7 +145,7 @@ educ_dist_plot <-  kz_data %>%
   geom_col(width = 0.8) +
   facet_wrap(~ km3_label, ncol = 1) +
   scale_x_continuous(labels = scales::percent_format()) +
-  scale_fill_manual(values = base_colors[1:3]) + 
+  scale_fill_manual(values = base_colors[c(1, 3, 5)]) + 
   labs(
     title = "Education level distribution by cluster",
     x = "",
@@ -138,16 +157,6 @@ educ_dist_plot <-  kz_data %>%
     plot.title = element_text(hjust = 0.5),
   )
 
-kz_data <- kz_data %>%
-  mutate(
-    urban_label = factor(
-      urbanicity,
-      levels = c(1, 2),
-      labels = c("Rural area", "Urban area")
-    )
-  )
-urban_colors <- get_gradient(length(unique(kz_data$urban_label)))
-
 urban_dist_plot <-  kz_data %>%
   group_by(km3_label, urban_label) %>%
   summarise(n = n(), .groups = "drop") %>%
@@ -157,7 +166,7 @@ urban_dist_plot <-  kz_data %>%
   geom_col(width = 0.8) +
   facet_wrap(~ km3_label, ncol = 1) +
   scale_x_continuous(labels = scales::percent_format()) +
-  scale_fill_manual(values = urban_colors) + 
+  scale_fill_manual(values = base_colors[c(4, 1)]) + 
   labs(
     title = "Urbanicity distribution by cluster",
     x = "",
@@ -169,35 +178,21 @@ urban_dist_plot <-  kz_data %>%
     plot.title = element_text(hjust = 0.5),
   )
 
-kz_data$fin24_label <- factor(
-  kz_data$fin24,
-  levels = c(1, 2, 3, 4, 5, 6, 7),
-  labels = c(
-    "Savings",
-    "Family/Relatives/Friends",
-    "Money from working",
-    "Borrowing (bank/employer/private)",
-    "Sale of assets",
-    "Other source",
-    "Could not come up with money"
-  )
-)
-
-fin24_dist_plot <-  kz_data %>%
-  group_by(km3_label, fin24_label) %>%
+emp_dist_plot <-  kz_data %>%
+  group_by(km3_label, emp_label) %>%
   summarise(n = n(), .groups = "drop") %>%
   group_by(km3_label) %>%
   mutate(share = n / sum(n)) %>%
-  ggplot(aes(x = share, y = "", fill = fin24_label)) +
+  ggplot(aes(x = share, y = "", fill = emp_label)) +
   geom_col(width = 0.8) +
   facet_wrap(~ km3_label, ncol = 1) +
   scale_x_continuous(labels = scales::percent_format()) +
-  scale_fill_brewer(palette = "Set2") +
+  scale_fill_manual(values = base_colors[c(1, 3)]) + 
   labs(
-    title = "Main source of emergency money in 30 days",
+    title = "Employment distribution by cluster",
     x = "",
     y = "Share of respondents",
-    fill = "The source"
+    fill = "Eployment"
   ) +
   theme_minimal(base_size = 13) +
   theme(
@@ -208,7 +203,7 @@ ggsave(
   filename = "plots/age_distribution_plot.png",
   plot = age_dist_plot,
   width = 8,
-  height = 6,
+  height = 4,
   dpi = 300
 )
 
@@ -216,7 +211,7 @@ ggsave(
   filename = "plots/inc_distribution_plot.png",
   plot = income_dist_plot,
   width = 8,
-  height = 6,
+  height = 4,
   dpi = 300
 )
 
@@ -224,6 +219,24 @@ ggsave(
   filename = "plots/educ_distribution_plot.png",
   plot = educ_dist_plot,
   width = 8,
-  height = 6,
+  height = 4,
   dpi = 300
 )
+
+
+ggsave(
+  filename = "plots/emp_distribution_plot.png",
+  plot = emp_dist_plot,
+  width = 8,
+  height = 4,
+  dpi = 300
+)
+
+ggsave(
+  filename = "plots/urban_distribution_plot.png",
+  plot = urban_dist_plot,
+  width = 8,
+  height = 4,
+  dpi = 300
+)
+
